b0f9df50b40bce138b91e6eaab90e8fbe93d0cfa
enubuzor <enubuzorchinedu@gmail.com>
2023-11-15 15:23:15 -0800

    simple

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /*
- * main - 
+ * main -
  * @c
  * Return: successful 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *prompt = "<simple_shell>$ ", *buffer;
+	const char *delim = (argc > 1) ? argv[1] : " ";
+	char *prompt = "<simple_shell>$ ", *buffer, *token;
 	size_t size = 0;
 	ssize_t num_chars;
 
@@ -17,12 +18,12 @@ int main(void)
 		printf("%s", prompt);
 		/* read user's command */
 		num_chars = getline(&buffer, &size, stdin);
-		
+
 		if (num_chars == -1)
 		{
 			/*check for end of file (Ctrl+D)*/
 			if (feof(stdin))
-			{	
+			{
 				printf("Exiting <simple_shell>$ ......\n");
 				free(buffer); /*free memory before returning*/
 				return (0);
@@ -36,14 +37,48 @@ int main(void)
 			}
 		}
 		/*remove the newline character from the command*/
-		if (buffer[num_chars -1] == '\n')
+		if (buffer[num_chars - 1] == '\n')
 		{
-			buffer[num_chars -1] = '\0';
+			buffer[num_chars - 1] = '\0';
 		}
+		/* tokenization of input */
+		token = strtok(buffer, delim);
 
-		printf("%s\n", buffer);
+		while (token != NULL)
+		{
+			/* process each command */
+			printf("%s\n", token);
+
+			/*get the next token*/
+			token = strtok(NULL, delim);
+		}
+		/*printf("%s\n", buffer);*/
+
+		/* fork and execute command */
+		pid_t pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork failed");
+			free(buffer);
+			return (-1);
+		}
+		else if (pid == 0)
+		{
+			/* using execve to execute command*/
+			if (execve(buffer, (char *[])[buffer, NULL], NULL) == -1)
+			{
+				perror("execve failed");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);/*wait fot the child process to complete*/
+		}
 	}
+
 	free(buffer);
-	
+
 	return (0);
 }
--- main.h
+++ main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-int main(void);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
